59c6a3b237df0c9da13693f2b6e59139bad73cde
Flor <Flor@egg.com>
2023-06-07 19:58:49 -0300

    repositorio para github

---
+++ js/script2.js
@@ -0,0 +1,9 @@
+// Función para mostrar destinos al hacer clic en el botón "Ver Destinos"
+function showDestinations() {
+    alert("¡Próximamente se agregarán más destinos!");
+  }
+  
+  // Función para mostrar un mensaje de contacto al hacer clic en el botón "Contactar"
+  function contactUs() {
+    alert("¡Gracias por tu interés! Nos pondremos en contacto contigo pronto.");
+  }
